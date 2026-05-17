dff5bf92010f5d8afcdd2fe48718c3268fc1aa11
vitaliy bondarenko <bovile17051992@gmail.com>
2026-05-17 14:22:49 +0300

    Format output as MySQL-style table with row numbers

--- a.php
+++ a.php
@@ -6,6 +6,16 @@ $name = trim(fgets(STDIN));
 echo "Скільки разів вивести? ";
 $num = (int) trim(fgets(STDIN));
 
-for ($i = 0; $i < $num; $i++) {
-    echo $name . PHP_EOL;
+$numWidth  = strlen((string) $num);
+$nameWidth = max(strlen($name), 4);
+$line = '+' . str_repeat('-', $numWidth + 2) . '+' . str_repeat('-', $nameWidth + 2) . '+';
+
+echo $line . PHP_EOL;
+echo '| ' . str_pad('#', $numWidth) . ' | ' . str_pad('Name', $nameWidth) . ' |' . PHP_EOL;
+echo $line . PHP_EOL;
+
+for ($i = 1; $i <= $num; $i++) {
+    echo '| ' . str_pad($i, $numWidth) . ' | ' . str_pad($name, $nameWidth) . ' |' . PHP_EOL;
 }
+
+echo $line . PHP_EOL;
